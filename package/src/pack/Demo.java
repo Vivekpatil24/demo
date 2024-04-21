413319a90fb65ef755697d582996fdace68b7e99
Vivekpatil24 <vivekpatil211002@gmail.com>
2024-04-21 22:08:53 +0530

    initail commit

---
+++ package/src/pack/Demo.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pack;
+
+
+public class Demo {
+    public void show(){
+    
+    System.out.println("this is inside package");
+    }
+    
+}
